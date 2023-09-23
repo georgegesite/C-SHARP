6c6dfd523e0d31018877c388c0a2595ce1e37617
George Gesite <georgesite33@gmail.com>
2023-09-23 20:54:22 +0800

    udpte

--- app/Program.cs
+++ app/Program.cs
@@ -30,5 +30,26 @@ class Program
         int n3 = 4;
         n3 = Null;
         Console.WriteLine("n3 = " + n3); // n3 = null
+
+        // If else Statements
+        int n = 1; // Initialize variable n with the value 1
+
+        // If statement without curly braces for single statements
+        if (n > 2)
+            n -= 1; // Subtract 1 from n if n is greater than 2
+        else if (n == 2)
+            n *= 2; // Multiply n by 2 if n is equal to 2
+        else
+            n += 2; // Add 2 to n otherwise
+
+        int m = 2; // Initialize variable m with the value 2
+
+        // Complex condition with parentheses for readability
+        if ((n > 2 && n != m) || n == m)
+        {
+            n += 1; // Add 1 to n if the condition is true
+        }
+
+        Console.WriteLine("n = " + n);
     }
 }
