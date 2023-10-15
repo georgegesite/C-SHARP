940a16464f67a232e25f56e585bc1e3cb713110e
George Gesite <georgesite33@gmail.com>
2023-10-16 06:45:30 +0800

    WriteLine
    Learning c# no 1

--- webapp/LearningCSharp/Program.cs
+++ webapp/LearningCSharp/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!"); // the next will be printed on new line
+Console.Write("This is not a new line"); //the next will be printed on same line
+Console.WriteLine("This is a new line");
+Console.Write("This is not a new line");
