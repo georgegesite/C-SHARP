095a8e7acfa168e3cf16a431c509329d70102bdc
George Gesite <georgesite33@gmail.com>
2025-01-09 11:14:44 +0800

    Updates

---
+++ Console/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string message = "Hello World!";
+            byte number1 = 1;
+            short number2 = 2;
+            int number3 = 150;
+            long number4 = 500;
+            float number5 = 1000f;
+            double number6 = 5000.00;
+            char char1 = 'E';
+            bool isDone = true;
+
+            Console.WriteLine(number6 + char1);
+            Console.WriteLine(isDone);
+        }
+    }
+}
