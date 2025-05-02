f8042b51bebcfb15494a48ffc61cd7660e668dec
George Gesite <georgesite33@gmail.com>
2025-05-02 20:07:47 +0800

    experimenting with c#

---
+++ Console/LearningCShapr/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myNum = 5;               // Integer (whole number)
+            double myDoubleNum = 5.99D;  // Floating point number
+            char myLetter = 'D';         // Character
+            bool myBool = true;          // Boolean
+            string myText = "Hello";     // String
+
+            Console.WriteLine(myText);
+
+            float f1 = 35e3F;
+            float f2 = 35E3F;
+            double d1 = 12E4D;
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+            Console.WriteLine(d1);
+        }
+    }
+}
