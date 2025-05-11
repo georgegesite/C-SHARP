4440001afad8480c608c46d428165b4ff1c73b39
George <georgesite33@gmail.com>
2025-05-11 21:24:40 +0800

    fun

--- Console/LearningCShapr/Program.cs
+++ Console/LearningCShapr/Program.cs
@@ -24,11 +24,56 @@ namespace HelloWorld
             Console.WriteLine(f2);
             Console.WriteLine(d1);
 
-            if(EMPTY_STRING.Equals("123")){
+            if (EMPTY_STRING.Equals("123"))
+            {
                 Console.WriteLine("is equal");
-            } else {
+            }
+            else if ("Hello".Equals(myText))
+            {
+                Console.WriteLine("Hello is equal");
+            }
+            else
+            {
                 Console.WriteLine("not equal");
             }
+
+
+            // Ternary Operators
+            int time = 20;
+            string result = (time < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
+
+            Console.WriteLine(calculateDay(7));
+        }
+
+        public static string calculateDay(int day)
+        {
+            string dayStr = EMPTY_STRING;
+            switch (day)
+            {
+                case 1:
+                    dayStr = "Monday";
+                    break;
+                case 2:
+                    dayStr = "Tuesday";
+                    break;
+                case 3:
+                    dayStr = "Wednesday";
+                    break;
+                case 4:
+                    dayStr = "Thursday";
+                    break;
+                case 5:
+                    dayStr = "Friday";
+                    break;
+                case 6:
+                    dayStr = "Saturday";
+                    break;
+                case 7:
+                    dayStr = "Sunday";
+                    break;
+            }
+            return dayStr;
         }
     }
 }
